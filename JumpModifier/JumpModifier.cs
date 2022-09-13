0d0e727206fcb2e9059a76e393323a83bc6c6188
Andrew-Zakharov <andrey.zakharow@keemail.me>
2022-09-13 18:33:36 +0200

    added code to modify jump force

--- JumpModifier/JumpModifier.cs
+++ JumpModifier/JumpModifier.cs
@@ -1,7 +1,29 @@
-﻿namespace JumpModifier
-{
-    public class Class1
-    {
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine;
 
+namespace JumpModifier
+{
+    [BepInPlugin("null.JumpModifier", "Jump Modifier", "1.0.0")]
+    [BepInProcess("valheim.exe")]
+    public class JumpModifier : BaseUnityPlugin
+    {
+        private readonly Harmony harmony = new Harmony("null.JumpModifier");
+
+        void Awake()
+        {
+            harmony.PatchAll();
+        }
+
+        [HarmonyPatch(typeof(Character), nameof(Character.Jump))]
+        class Jump_Patch
+        {
+            static void Prefix(ref float ___m_jumpForce)
+            {
+                Debug.Log($"Jump force: {___m_jumpForce}");
+                ___m_jumpForce = 15;
+                Debug.Log($"Modified jump force: {___m_jumpForce}");
+            }
+        }
     }
 }
