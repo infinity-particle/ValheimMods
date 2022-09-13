0275764c84175107dd6c0e23702f891a95638665
Andrew-Zakharov <andrey.zakharow@keemail.me>
2022-09-13 21:57:46 +0200

    added ContainerSort project

---
+++ ContainerSort/Class1.cs
@@ -0,0 +1,19 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System.Diagnostics;
+using UnityEngine;
+
+namespace ContainerSort
+{
+    [BepInPlugin("null.ContainerSort", "Container Sort", "1.0.0")]
+    [BepInProcess("valheim.exe")]
+    public class ContainerSort : BaseUnityPlugin
+    {
+        private readonly Harmony harmony = new Harmony("null.ContainerSort");
+
+        void Awake()
+        {
+            harmony.PatchAll();
+        }
+    }
+}
